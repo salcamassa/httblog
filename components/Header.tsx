56c9b1997676b088bd07ace94eda44e508dad3c0
sal <19803445+salcamassa@users.noreply.github.com>
2023-12-04 18:24:19 -0500

    prettier fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,10 @@
-import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo-light.svg'
 import logoimg from '@/data/logo-light.png'
+import siteMetadata from '@/data/siteMetadata'
+import Image from 'next/image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import SearchButton from './SearchButton'
-import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -15,7 +13,7 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Image className="logo" src={logoimg}  alt="original logo" />
+              <Image className="logo" src={logoimg} alt="original logo" />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
